19eeae3f4030f07b9dd38d0bd585803c32377dfd
Marco Celoria <celoria.marco@gmail.com>
2024-06-03 10:34:52 +0200

    fix onnx check

--- src/check_onnx.py
+++
@@ -1,9 +0,0 @@
-import onnx
-input_file="results/S1465_ProstT5/snapshots/onnx/ProstT5_Finetuning.onnx"
-print(onnx.checker.check_model(input_file))
-
-input_file="results/S1465_MSA/snapshots/onnx/MSA_Finetuning.onnx"
-print(onnx.checker.check_model(input_file))
-
-input_file="results/S1465_ESM2/snapshots/onnx/ESM2_Finetuning.onnx"
-print(onnx.checker.check_model(input_file))
---
+++ src/onnx_inspector.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from pathlib import Path
+import onnx
+from argparser import *
+
+
+def main(onnx_path, output_dir):
+    result_dir    = output_dir + "/results"
+    if not(os.path.exists(output_dir) and os.path.isdir(output_dir)):
+        os.makedirs(output_dir)
+    if not(os.path.exists(result_dir) and os.path.isdir(result_dir)):
+        os.makedirs(result_dir)
+    print(f"Checking onnx file: {onnx_path}")
+    # Check the model
+    try:
+        onnx.checker.check_model(onnx_path, full_check=True)
+    except onnx.checker.ValidationError as e:
+        print("The model is invalid: %s" % e)
+    else:
+        print("The model is valid!")
+
+if __name__ == "__main__":
+    args = argparser_onnx_inspector()
+    target = args.config_file
+    target_path = Path(target)
+    if not os.path.exists(target_path):
+        print(f"The path {target_path} doesn't exist")
+        raise SystemExit(1)
+
+    if os.path.isfile(target_path):
+        config = load_config(target_path)
+        try:
+            onnx_file = config["onnx_file"]
+            onnx_path = Path(onnx_file)
+            if not os.path.exists(onnx_path):
+                print(f"The onnx path {onnx_path} doesn't exist")
+                raise SystemExit(1)
+            if not os.path.isfile(onnx_path):
+                print(f"The onnx path {onnx_path} is not a file")
+                raise SystemExit(1)
+        except:
+            print(f"The onnx file doesn't exist")
+            raise SystemExit(1)
+
+        try:
+            output_dir = config["output_dir"]
+        except:
+            output_dir = os.getcwd()
+            print(f"Setting the default output directory: {output_dir}")
+
+    main(onnx_path, output_dir)
